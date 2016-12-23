f9be1b42509dbba514145c2126c05414119798ee
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2016-12-23 16:31:29 +0330

    Search and Get movies by list id

---
+++ index.js
@@ -0,0 +1,66 @@
+const omdb = require('omdb');
+
+class Imdb {
+  constructor() {
+    this.movies = [];
+  }
+
+  search(name = '', year = '', type = '') {
+    this.movies = [];
+
+    let pattern = {
+      terms: name,
+      year: year,
+      type: type
+    };
+
+    return new Promise((resolve, reject) => {
+      omdb.search(pattern, (err, movies) => {
+          if (err) {
+            reject(err);
+          }
+
+          if (!movies.length) {
+            reject('Movie not found!');
+          }
+
+          for (let i = 0; i < movies.length; i += 1) {
+            movies[i].id = i;
+            this.movies.push(movies[i]);
+          }
+
+          resolve(this.movies);
+        });
+    })
+  }
+
+  getImdb(id) {
+    let imdb = this.movies.filter(movie => movie.id === id);
+
+    if (imdb.length) {
+      return imdb[0].imdb;
+    }
+
+    return 0;
+  }
+
+  get(id) {
+    let imdb = this.getImdb(id);
+    let options = { fullPlot: true, tomatoes: true };
+
+    return new Promise((resolve, reject) => {
+      omdb.get({ imdb: imdb }, options, (err, movie) => {
+          if (err) {
+            reject(err);
+          }
+
+          if (!movie) {
+            reject('Movie not found!');
+          }
+
+          resolve(movie);
+        });
+    });
+  }
+
+}
